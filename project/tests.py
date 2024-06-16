58589468b6ce9d63ea631bb385ad5da36e948706
Alexander <alexandradaniloff@gmail.ru>
2024-06-16 21:10:36 +0300

    добавлены тесты пользователя

--- project/tests.py
+++ project/tests.py
@@ -87,3 +87,114 @@ class PerevalTest(APITestCase):
         serializer_data = PerevalSerializer(self.pereval_2).data
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(serializer_data, response.data)
+
+    def test_valid_create_and_user_reuse(self):
+        data = {
+        "beauty_title": "title_9",
+        "title": "title_1",
+        "other_titles": "title_1",
+        "connect": "title_1",
+        "tourist_id": {
+
+            "email": "test1@mail.ru",
+            "last_name": "Test1",
+            "first_name": "Test1",
+            "patronymic": "Test1",
+            "phone": "79998837756"
+        },
+        "coord_id": {
+
+            "latitude": "54.87690019",
+            "longitude": "43.36759009",
+            "height": 1244
+        },
+        "level": {
+
+            "winter_lev": "4A",
+            "spring_lev": "2A",
+            "summer_lev": "1A",
+            "autumn_lev": "3A"
+        },
+        "images": []
+    }
+
+        response = self.client.post(reverse('perevals-list'), data=json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Pereval.objects.all().count(), 3)
+        self.assertEqual(Users.objects.all().count(), 2)
+
+    def test_invalid_create(self):
+        data = {
+        "beauty_title": "",
+        "title": "",
+        "other_titles": "",
+        "connect": "",
+        "tourist_id": {
+
+            "email": "",
+            "last_name": "",
+            "first_name": "",
+            "patronymic": "Иванович",
+            "phone": "2222222223"
+        },
+        "coord_id": {
+
+            "latitude": "54.87690019",
+            "longitude": "43.36759009",
+            "height": 1244
+        },
+        "level": {
+
+            "winter_lev": "4A",
+            "spring_lev": "2A",
+            "summer_lev": "1A",
+            "autumn_lev": "3A"
+        },
+        "images": []
+    }
+
+        json_data = json.dumps(data)
+        response = self.client.post(reverse('perevals-list'), data=json_data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Pereval.objects.all().count(), 2)
+
+# проверяем отсутствие обновления разрешенных данных, если статус не new
+    def test_invalid_update_if_status_is_not_new(self):
+        data = {
+        "status": "PN",
+        "beauty_title": "title_10",
+        "title": "title_1",
+        "other_titles": "title_1",
+        "connect": "",
+        "tourist_id": {
+
+            "email": "ivanov@gmail.ru",
+            "last_name": "Иванов",
+            "first_name": "Иван",
+            "patronymic": "Иванович",
+            "phone": "2222222223"
+        },
+        "coord_id": {
+
+            "latitude": "54.87690019",
+            "longitude": "43.36759009",
+            "height": 1244
+        },
+        "level": {
+
+            "winter_lev": "4A",
+            "spring_lev": "2A",
+            "summer_lev": "1A",
+            "autumn_lev": "3A"
+        },
+        "images": []
+    }
+
+        json_data = json.dumps(data)
+        response = self.client.patch(reverse('perevals-detail', kwargs={'pk': self.pereval_2.id}),
+            data=json_data,
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.pereval_2.refresh_from_db()
+        self.assertEqual('', self.pereval_2.connect)
