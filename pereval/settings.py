01f5897e40c08a14656466ac4b15ac20d3c27934
Alexander <alexandradaniloff@gmail.ru>
2024-06-17 00:29:34 +0300

    добавлены настройки к postgres

--- pereval/settings.py
+++ pereval/settings.py
@@ -87,27 +87,27 @@ WSGI_APPLICATION = 'pereval.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-         'USER': os.getenv('FSTR_DB_LOGIN'),
-         'PASSWORD': os.getenv('FSTR_DB_PASS'),
-         'HOST': os.getenv('FSTR_DB_HOST'),
-         'PORT': os.getenv('FSTR_DB_PORT'),
-     }
- }
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'postgres',
-#         'USER': os.getenv('FSTR_DB_LOGIN'),
-#         'PASSWORD': os.getenv('FSTR_DB_PASS'),
-#         'HOST': os.getenv('FSTR_DB_HOST'),
-#         'PORT': os.getenv('FSTR_DB_PORT'),
-#     }
-# }
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': BASE_DIR / 'db.sqlite3',
+#          'USER': os.getenv('FSTR_DB_LOGIN'),
+#          'PASSWORD': os.getenv('FSTR_DB_PASS'),
+#          'HOST': os.getenv('FSTR_DB_HOST'),
+#          'PORT': os.getenv('FSTR_DB_PORT'),
+#      }
+#  }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': os.getenv('FSTR_DB_LOGIN'),
+        'PASSWORD': os.getenv('FSTR_DB_PASS'),
+        'HOST': os.getenv('FSTR_DB_HOST'),
+        'PORT': os.getenv('FSTR_DB_PORT'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
